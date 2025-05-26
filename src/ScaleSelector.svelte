d8dc1bb57eaf71e999cf06d8fb639ed9ec7cce2a
nilsmango <simonmilagrolang@icloud.com>
2025-05-26 16:09:49 +0200

    Added +/- buttons to change they scale recipe by clicking on those.

--- src/ScaleSelector.svelte
+++ src/ScaleSelector.svelte
@@ -1,24 +1,24 @@
 <script lang="ts">
 	import Fraction from "fraction.js";
-
 	export let scaleNum: number | null = 1;
 	export let scale;
 	$: scale = new Fraction(scaleNum || 1);
 </script>
-
 <div>
-	<label
-		>Scale recipe <input
+	<label>
+		Scale recipe
+		<input
 			type="number"
 			inputmode="decimal"
-			min="0"
+			min="1"
 			on:blur={() => (scaleNum = scaleNum || 1)}
 			on:focus={() => (scaleNum = scaleNum == 1 ? null : scaleNum)}
 			bind:value={scaleNum}
-		/></label
-	>
+		/>
+		<button on:click={() => scaleNum = Math.max((scaleNum || 1) - 1, 1)}>-</button>
+		<button on:click={() => scaleNum = (scaleNum || 1) + 1}>+</button>
+	</label>
 </div>
-
 <style>
 	div {
 		width: 100%;
@@ -30,7 +30,6 @@
 		border-radius: var(--radius-s);
 		color: var(--text-muted);
 	}
-
 	input {
 		width: var(--size-4-16);
 		border: none;
@@ -45,16 +44,24 @@
 		font-size: var(--font-smaller);
 		height: var(--size-4-8);
 	}
-
 	label {
 		margin-right: calc(-1 * var(--size-4-2));
 	}
-
 	label::after {
 		content: "×";
 		position: relative;
 		width: var(--size-4-4);
-		right: calc(var(--size-4-16) - var(--size-4-2));
+		right: calc(var(--size-4-16) + var(--size-4-8) + var(--size-4-8));
 		color: var(--text-accent);
 	}
+	
+	button {
+	background: var(--background-modifier-hover);
+	border: none;
+	margin: 0 var(--size-4-1);
+	cursor: pointer;
+	font-size: var(--font-smaller);
+	padding: 0 var(--size-4-2);
+	border-radius: var(--radius-s);
+}
 </style>
